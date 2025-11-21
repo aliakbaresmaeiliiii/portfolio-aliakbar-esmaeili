d1d94144a3303e364396b7d646379616d0e4cc78
aliakbaresmaeiliiii <aliakbaresmaeili98@gmail.com>
2025-11-21 14:53:51 +0800

    Add Firebase Hosting workflows

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,11 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
-import { importProvidersFrom } from '@angular/core';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter([]),
-    importProvidersFrom(BrowserAnimationsModule)
+    provideAnimations()
   ]
 }).catch(err => console.error(err));
